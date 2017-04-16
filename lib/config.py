8581b00178211e8d19c4b13b27283e19d2ee3467
Ted Stein <me@tedstein.net>
2017-04-16 13:40:45 -0700

    laundryd: Initial commit.

--- lib/config.py
+++ lib/config.py
@@ -1,5 +1,9 @@
 from collections import defaultdict
 from socket import gethostname
+from sys import argv
+
+def service_name():
+    return argv[0].split("/")[-1]
 
 def bykey(key, default, **kwargs):
     return defaultdict(lambda: default, **kwargs)[key]
