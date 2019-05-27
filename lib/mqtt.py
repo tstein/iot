2e1f1f877b5c3cc9c116f2b548ba3952b3e75d0c
Ted Stein <me@tedstein.net>
2019-05-26 17:59:23 -0700

    mqtt: Add a subscribe hook.

--- lib/mqtt.py
+++ lib/mqtt.py
@@ -6,6 +6,7 @@ import paho.mqtt.client
 
 _MQTTC = None
 _PUBLISH_CALLBACKS = {}
+_SUBSCRIBE_CALLBACKS = {}
 
 server = 'bill'
 
@@ -23,12 +24,25 @@ def start_client(service_name):
 
     client_name = "{}-{}".format(socket.gethostname(), service_name)
     _MQTTC = paho.mqtt.client.Client(client_name)
+    _MQTTC.on_message = _on_message
     _MQTTC.connect(server)
     _MQTTC.loop_start()
     Thread(target=publisher(_MQTTC), daemon=True).start()
 
 def publish_forever(topic, callback):
     _PUBLISH_CALLBACKS[topic] = callback
+
+def subscribe(topic, callback):
+    _SUBSCRIBE_CALLBACKS[topic] = callback
+    _MQTTC.subscribe(topic)
+
+def _on_message(client, userdata, msg):
+    topic = msg.topic
+    payload = msg.payload.decode("utf-8")
+    if topic in _SUBSCRIBE_CALLBACKS:
+        _SUBSCRIBE_CALLBACKS[topic](payload)
+    else:
+        print("unexpected message on {}: {}".format(topic, payload))
 
 def tell(whom, what, client=None):
     if not client:
